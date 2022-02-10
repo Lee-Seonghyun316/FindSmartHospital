22ad17c556d53f1332d0e33b67f55c2a9b96b8b3
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-10 22:47:30 +0900

    Style: App.js Theme 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import { getUserInfo } from "./firebase";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegionalHospitals from "./components/RegionalHospitals";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import "./App.css";
 
 function App() {
-  const provider = new GoogleAuthProvider();
-  const auth = getAuth();
-  const signInGoogle = () => {
+  const signInGoogle = useCallback(() => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
     signInWithRedirect(auth, provider);
     getUserInfo(auth);
-  };
+  }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/region" element={<RegionalHospitals />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/region" element={<RegionalHospitals />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
