2bb407acac7bfdf87da25ede1e7aab24ddba12be
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-10 18:02:17 +0900

    Feat: firebase google login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { getUserInfo } from "./firebase";
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  signInWithRedirect(auth, provider);
+  getUserInfo(auth);
+
   return <div>helloWorld</div>;
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,4 +13,29 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+//auth
+export const getUserInfo = (auth) => {
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      console.log(token);
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
