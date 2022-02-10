2a8a98f6702b0c4f51ae2789b9e6c13fdf0db636
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-10 17:53:14 +0900

    Setting: firebase config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCspx-9yJTX0zapRca3Z9U_WNOwYkyy-As",
+  authDomain: "findsmarthospital.firebaseapp.com",
+  projectId: "findsmarthospital",
+  storageBucket: "findsmarthospital.appspot.com",
+  messagingSenderId: "411261757194",
+  appId: "1:411261757194:web:bb75ca4b8a4922df71bcf3",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
